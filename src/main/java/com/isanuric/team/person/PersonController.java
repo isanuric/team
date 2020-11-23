3fb5c51f5640db5b847fc5cb4708884cf1258169
Ehsan Salmani <difference@tutanota.com>
2020-11-23 20:21:18 +0100

    Adjust secret

--- src/main/java/com/isanuric/team/person/PersonController.java
+++ src/main/java/com/isanuric/team/person/PersonController.java
@@ -2,12 +2,23 @@ package com.isanuric.team.person;
 
 
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
+
+    private final Logger logger = LoggerFactory.getLogger(PersonController.class);
+
+    @Value("${spring.user.name}")
+    private String username;
+
+    @Value("${bean.message}")
+    private String beanMessage;
 
     private final PersonRepository personRepository;
 
@@ -23,6 +34,9 @@ public class PersonController {
 
     @GetMapping("/info")
     public String info() {
+        System.out.println("username: " + username);
+        logger.info("username: [{}]", username);
+        logger.info("beanMessage: [{}]", beanMessage);
         return "Info";
     }
 
